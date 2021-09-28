b44a97614564396aba6bb67c3f27d2dd1502eb23
Marco Polato <m.polato@kuama.net>
2021-09-28 17:14:58 +0200

    last suggestions

--- lib/src/shared/widgets/dialogs/app_settings_dialog.dart
+++ lib/src/shared/widgets/dialogs/app_settings_dialog.dart
@@ -1,34 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:kuama_flutter/src/_utils/lg.dart';
-import 'package:kuama_flutter/src/features/app_pages/use_cases/open_settings_app_page.dart';
+import 'package:kuama_flutter/src/features/positioner/usecases/open_position_service_page.dart';
 import 'package:kuama_flutter/src/shared/feature_structure/use_case.dart';
 
-class AskOpenAppSettingsPageDialog extends StatelessWidget {
+class AskOpenSettingsPageDialog extends StatelessWidget {
+  final UseCase<NoParams, bool>? openSettings;
   final Widget? title;
   final Widget? cancelLabel;
   final Widget? settingsLabel;
 
-  const AskOpenAppSettingsPageDialog({
+  const AskOpenSettingsPageDialog({
     Key? key,
+    this.openSettings,
     this.title,
     this.cancelLabel,
     this.settingsLabel,
   }) : super(key: key);
 
   void openSettingsPage() async {
-    final res = await GetIt.I<OpenSettingsAppPage>().call(NoParams());
+    final res = openSettings != null
+        ? await openSettings!.call(NoParams())
+        : await GetIt.I<OpenPositionServicePage>().call(NoParams());
     res.fold((failure) {
-      lg.e('The app settings page could not be opened', failure);
+      lg.e('The settings page could not be opened', failure);
     }, (wasOpened) {
-      if (!wasOpened) lg.e('The app settings page could not be opened');
+      if (!wasOpened) lg.e('The settings page could not be opened');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: title ?? const Text('Open app settings'),
+      title: title ?? const Text('Open settings'),
       actions: [
         TextButton(
           onPressed: () => Navigator.of(context).pop(false),
@@ -36,7 +40,7 @@ class AskOpenAppSettingsPageDialog extends StatelessWidget {
         ),
         ElevatedButton(
           onPressed: openSettingsPage,
-          child: settingsLabel ?? const Text('Settings'),
+          child: settingsLabel ?? const Text('Open'),
         ),
       ],
     );
