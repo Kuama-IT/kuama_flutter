bbc99841d4c9ca1cdfb74e676a717ceb36010cfa
Marco Polato <m.polato@kuama.net>
2021-09-29 14:24:34 +0200

    fix on last suggestions

--- lib/src/shared/widgets/dialogs/app_settings_dialog.dart
+++ lib/src/shared/widgets/dialogs/app_settings_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:kuama_flutter/src/_utils/lg.dart';
-import 'package:kuama_flutter/src/features/positioner/usecases/open_position_service_page.dart';
+import 'package:kuama_flutter/src/features/app_pages/use_cases/open_settings_app_page.dart';
 import 'package:kuama_flutter/src/shared/feature_structure/use_case.dart';
 
 class AskOpenSettingsPageDialog extends StatelessWidget {
@@ -19,9 +19,7 @@ class AskOpenSettingsPageDialog extends StatelessWidget {
   }) : super(key: key);
 
   void openSettingsPage() async {
-    final res = openSettings != null
-        ? await openSettings!.call(NoParams())
-        : await GetIt.I<OpenPositionServicePage>().call(NoParams());
+    final res = await (openSettings ?? GetIt.I<OpenSettingsAppPage>()).call(NoParams());
     res.fold((failure) {
       lg.e('The settings page could not be opened', failure);
     }, (wasOpened) {
